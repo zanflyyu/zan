f8224cb5b9f88cafa1375641fb85b7482999fff0
zanflyyu <zanflyyu@gmail.com>
2015-09-23 14:33:40 +0800

    The first java program

---
+++ src/HelloJava.java
@@ -0,0 +1,15 @@
+
+public class HelloJava {
+
+	/**
+	 * @param args
+	 */
+	private static String say="我要学会你";
+	
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		System.out.println("你好，Java World!"+say);
+
+	}
+
+}
---
+++ src/Number.java
@@ -0,0 +1,17 @@
+
+public class Number {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		byte mybyte =124;
+		short myshort=32564;
+		int myint = 45784612;
+		long mylong = 46789451L;
+		long result = mybyte+myshort+myint+mylong;
+		System.out.println("四种类型相加的结果为："+result);
+	}
+
+}
